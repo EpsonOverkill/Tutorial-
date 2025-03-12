36baaaee8c88a2f0b61980c5ac7d4b9817f51ad2
EpsonOverkill <140883362+EpsonOverkill@users.noreply.github.com>
2025-03-13 00:14:28 +0530

    Update 1.1.py

--- Tutorial 1/1.1.py
+++ Tutorial 1/1.1.py
@@ -1,9 +1,5 @@
-def stoh(seconds):
-    hours=seconds//3600
-    minutes=(seconds%3600)//60
-    seconds=seconds%60
-    return f"{hours:02}:{minutes:02}:{seconds:02}"
-
-seconds=int(input("Enter the time in seconds: "))
-actual_time=stoh(seconds)
-print("Time in HH:MM:SS format",actual_time)
+sec = int(input("Enter time in seconds: "))
+hrs = sec//3600
+min = (sec%3600)//60
+sec = sec%60
+print(f"{hrs:02}:{min:02}:{sec:02}")
