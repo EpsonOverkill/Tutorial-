7795df9a336501e5598425ecf419dfaedfe811c3
EpsonOverkill <140883362+EpsonOverkill@users.noreply.github.com>
2025-03-14 12:54:00 +0530

    Update and rename P26.py to P27.py

--- T2/P27.py
+++ T2/P27.py
@@ -1,4 +1,3 @@
-# Separate primes and composites
 def is_p(n): return n > 1 and all(n % i for i in range(2, int(n**0.5) + 1))
 nums = list(map(int, input("Enter numbers: ").split()))
 print("Primes:", [n for n in nums if is_p(n)], "Composites:", [n for n in nums if not is_p(n)])
