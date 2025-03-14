81b3fd5677c9caa40a2f5fc5be001b993da69087
EpsonOverkill <140883362+EpsonOverkill@users.noreply.github.com>
2025-03-14 12:48:38 +0530

    Update and rename P10.py to P10&P11.py

--- T2/P10&P11.py
+++ T2/P10&P11.py
@@ -1,4 +1,3 @@
-# Validate password
 import re
 p = input("Enter password: ")
 print("Valid" if len(p) >= 6 and re.search(r'[a-z]', p) and re.search(r'[A-Z]', p) and re.search(r'\d', p) and re.search(r'[$#@]', p) else "Invalid")
