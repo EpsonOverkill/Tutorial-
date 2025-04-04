a3999adbb36eaa1cc7df968484d8334e9ae663fb
EpsonOverkill <140883362+EpsonOverkill@users.noreply.github.com>
2025-04-04 06:11:45 +0530

    Update P05.py

--- T3/P05.py
+++ T3/P05.py
@@ -1,10 +1,10 @@
 import turtle
 t = turtle.Turtle()
 t.speed(2)
-def hex(size):
+def hexa(size):
     for _ in range(6):
         t.forward(size)
         t.right(60)
 for _ in range(10):
-    hex(150)
+    hexa(150)
     t.right(36)
