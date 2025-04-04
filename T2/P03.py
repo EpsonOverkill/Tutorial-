c8841ce92ea56ddf4e6e7a9f85bb730b45f2eebd
EpsonOverkill <140883362+EpsonOverkill@users.noreply.github.com>
2025-04-04 05:36:24 +0530

    Update P03.py

--- T2/P03.py
+++ T2/P03.py
@@ -1,2 +1,2 @@
-s = input("Enter string: ")
-print("Palindrome:", all(s[i] == s[-i-1] for i in range(len(s)//2)))
+str = input("Enter string: ")
+print("Palindrome:", all(s[i] == str[-i-1] for i in range(len(s)//2)))
